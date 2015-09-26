3e349ebb7a5addc7ea8a81a06e50967b94fba895
Shing Lyu <shing.lyu@gmail.com>
2015-09-26 10:20:55 +0800

    Added shootable ball

--- js/main.js
+++ js/main.js
@@ -3,9 +3,27 @@ Physijs.scripts.ammo = '../../bower_components/ammo.js/builds/ammo.js'
 
 //var scene = new THREE.Scene();
 var scene = new Physijs.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000  );
-var ambientLight = new THREE.AmbientLight( 0xffffff );
-scene.add( ambientLight);
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000  ); // ambient light
+am_light = new THREE.AmbientLight( 0x444444  );
+scene.add( am_light  );
+
+// directional light
+dir_light = new THREE.DirectionalLight( 0xffffff);
+dir_light.position.set( 20, 30, 10  );
+dir_light.target.position.copy( scene.position  );
+dir_light.castShadow = true;
+dir_light.shadowCameraLeft = -30;
+dir_light.shadowCameraTop = -30;
+dir_light.shadowCameraRight = 30;
+dir_light.shadowCameraBottom = 30;
+dir_light.shadowCameraNear = 20;
+dir_light.shadowCameraFar = 200;
+dir_light.shadowBias = -.001
+dir_light.shadowMapWidth = dir_light.shadowMapHeight = 2048;
+dir_light.shadowDarkness = .5;
+scene.add( dir_light  );
+//var ambientLight = new THREE.AmbientLight( 0xffffff );
+//scene.add( ambientLight);
 
 /*
 var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5  );
@@ -13,10 +31,12 @@ var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5  );
 directionalLight.position.set( 0, 0, 30  );
 scene.add( directionalLight  );
 */
+/*
 var pointLight = new THREE.PointLight( 0xffffff, 1, 100  );
 
 pointLight.position.set( 30, 0, 50  );
 scene.add( pointLight  );
+*/
 /*
 var directionalLight = new THREE.DirectionalLight( 0xffff00, 0.5  );
 
@@ -25,12 +45,23 @@ scene.add( directionalLight  );
 */
 
 var renderer = new THREE.WebGLRenderer();
+renderer.shadowMapEnabled = true;
 renderer.setSize( window.innerWidth, window.innerHeight  );
 document.body.appendChild( renderer.domElement  );
 
+var table_material = Physijs.createMaterial(
+  new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture( 'images/wood.jpg'  ), ambient: 0xFFFFFF  }),
+  .9, // high friction
+  .2 // low restitution
+
+);
+table_material.map.wrapS = table_material.map.wrapT = THREE.RepeatWrapping;
+table_material.map.repeat.set( 5, 5  );
 var ground = new Physijs.BoxMesh(
-  new THREE.BoxGeometry(100, 1, 100),
-  new THREE.MeshPhongMaterial( { color: 0x404040, specular: 0x009900, shininess: 30, shading: THREE.FlatShading  }  ),
+  new THREE.BoxGeometry(300, 1, 100),
+  //new THREE.MeshPhongMaterial( { color: 0x404040, specular: 0x009900, shininess: 30, shading: THREE.FlatShading  }  ),
+  //new THREE.MeshLambertMaterial( { color: 0x404040}  ),
+  table_material,
   0 // mass
 
 );
@@ -38,6 +69,20 @@ ground.receiveShadow = true;
 ground.position.set(0,-30,0);
 //ground.rotation.set(0,-20,-10);
 scene.add( ground  );
+
+var backboard = new Physijs.BoxMesh(
+  new THREE.BoxGeometry(100, 1, 100),
+  //new THREE.MeshPhongMaterial( { color: 0x404040, specular: 0x009900, shininess: 30, shading: THREE.FlatShading  }  ),
+  //new THREE.MeshLambertMaterial( { color: 0x404040}  ),
+  table_material,
+  0 // mass
+
+);
+backboard.receiveShadow = true;
+backboard.position.set(0,0,-5);
+backboard.rotation.set(0.5 * Math.PI, 0, 0);
+backboard.receiveShadow = true;
+scene.add( backboard);
 
 //var geometry = new THREE.BoxGeometry( 1, 1, 1  );
 //var material = new THREE.MeshBasicMaterial( { color: 0x00ff00  }  );
@@ -73,7 +118,7 @@ function drawPie(angFrom, angDelta, color){
   //nextAng = this.angPrev + angToMove;
 
   var extrudeOpts = { 
-    amount: 10, 
+    amount: 2, 
     //bevelEnabled: true, 
     bevelEnabled: false, 
     /*
@@ -88,13 +133,11 @@ function drawPie(angFrom, angDelta, color){
 
   // Creating the 3D object, positioning it and adding it to the scene
   //pieobj = new THREE.Mesh( geometry, material );
-  pieobj = new Physijs.BoxMesh( geometry, material );
+  pieobj = new Physijs.ConvexMesh( geometry, material );
   pieobj.rotation.set(0,0,angFrom);
   // Adds shadows if selected as an option
-  if( this.hasShadows ){
-    pieobj.castShadow = true;
-    pieobj.receiveShadow = true;
-  }
+  pieobj.castShadow = true;
+  pieobj.receiveShadow = true;
   return pieobj
 
 }
@@ -116,9 +159,25 @@ for (var i = 0; i < slicesCount; i++){
 //scene.add( cube  );
 
 //camera.position.z = 25;
-camera.position.set(0,30,60);
+camera.position.set(0,20,80);
 //camera.up = new THREE.Vector3(0,0,0);
-camera.lookAt(new THREE.Vector3(0,1,-1));
+//camera.lookAt(new THREE.Vector3(30,30,0));
+camera.lookAt(new THREE.Vector3(0,0,0));
+
+window.addEventListener('click', function(e){
+  var geometry = new THREE.SphereGeometry( 1, 32, 32  );
+  var material = new THREE.MeshPhongMaterial( { color: 0xff0000} );
+  //var material = new THREE.PhonMaterial( {color: 0xffff00}  );
+  var dart = new Physijs.SphereMesh( geometry, material );
+  //var sphere = new THREE.Mesh( geometry, material  );
+  dart.castShadow = true;
+  dart.receiveShadow = true;
+  dart.position.set(20,0,80)
+  scene.add( dart );
+  //dart.setLinearVelocity(new THREE.Vector3(0,10,1))
+  dart.setLinearVelocity({x: -10, y: 20, z: -50});
+  
+})
 
 var flag = false;
 var render = function () {
